2fcbd90f726e8119aa90a062a846bcf2678aa429
Wassim Akkari <was.akkari@gmail.com>
2024-09-20 11:54:18 +0100

    Deduplicate code and extract into a function

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut largest = number_list[0];
+    let result = largest(&number_list);
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}", largest);
+    println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let mut largest = number_list[0];
+    let result = largest(&number_list);
 
-    for number in number_list {
+    println!("The largest number is {}", result);
+}
+
+fn largest(nums: &[i32]) -> i32 {
+    let mut largest = nums[0];
+
+    for &number in nums.iter().skip(1) {
         if number > largest {
             largest = number;
         }
     }
 
-    println!("The largest number is {}", largest);
+    largest
 }
